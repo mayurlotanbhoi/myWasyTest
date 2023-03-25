1a803d7db9074dbc4ef4a6b28d0e92c78064e7a8
mayur bhoi <mayurbhoi200@gmail.com>
2023-03-25 21:05:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [email, setemail] = useState("");
+  const [names, setnames] = useState("");
+  const [numbers, setnumbers] = useState("");
+
+  const postApi = (postdata) => {
+    fetch(" https://test-api-v3.myways.ai/user", {
+      method: "post",
+      body: postdata,
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        alert("use Is Created");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const makeGate = (e) => {
+    e.preventDefault();
+    fetch("https://test-api-v3.myways.ai/user?email=" + email)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        const user = {
+          name: names,
+          email: email,
+          phone: numbers,
+        };
+        postApi(user);
+      })
+      .catch((error) => {
+        alert("user Not Found");
+        console.log("error");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={(e) => {
+          makeGate(e);
+        }}
+      >
+        name
+        <input
+          type="text"
+          value={names}
+          onChange={(e) => setnames(e.target.value)}
+        />
+        email
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setemail(e.target.value)}
+        />
+        Phone Number
+        <input
+          type="number"
+          value={numbers}
+          onChange={(e) => setnumbers(e.target.value)}
+        />
+        <input type="submit" value="submit from" />
+      </form>
     </div>
   );
 }
 
 export default App;
+
